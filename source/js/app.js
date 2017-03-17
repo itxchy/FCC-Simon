57802694cbb004f33fd6bfc9ab4019ac23a59072
Matt Trifilo <matt.trifilo@gmail.com>
2017-03-17 01:09:45 -0500

    fix lint errors and this references

--- source/js/app.js
+++ source/js/app.js
@@ -1,3 +1,5 @@
+/* global $ */
+
 /**
 * Simon in JavaScript.
 * Another Front End challenge for Free Code Camp.
@@ -7,87 +9,89 @@
 'strict mode';
 
 const audio = (function () {
+  /**
+   * Create a new audio context
+   */
+  const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+  const gainNode = audioCtx.createGain();
+  gainNode.connect(audioCtx.destination);
+  gainNode.gain.value = 0.01;
+  let currGain = gainNode.gain.value; // eslint-disable-line no-unused-vars
+  const waveType = 'sine';
 
-  // ******* Create a new audio context *******
-
-	const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-	const gainNode = audioCtx.createGain();
-	gainNode.connect(audioCtx.destination);
-	gainNode.gain.value = 0.01;
-	let currGain = gainNode.gain.value;
-	const waveType = 'sine';
-
-  // ******* Create oscillators for the four game buttons *******
-
+  /**
+   * Create oscillators for the four game buttons
+   */
   let initializeOscillator = (frequency) => {
     let oscillator = audioCtx.createOscillator();
     oscillator.type = waveType;
     oscillator.frequency.value = frequency;
-    oscillator.start(0)
-    return oscillator
-  }
+    oscillator.start(0);
+    return oscillator;
+  };
 
-  const nw = initializeOscillator(440)
-  const ne = initializeOscillator(554.37)
-  const sw = initializeOscillator(659.25)
-  const se = initializeOscillator(783.99)
+  const nw = initializeOscillator(440);
+  const ne = initializeOscillator(554.37);
+  const sw = initializeOscillator(659.25);
+  const se = initializeOscillator(783.99);
 
-  // ******* Methods for starting each audio tone ******* 
-  
+  /**
+   * Methods for starting each oscillator tone
+   */
   let startTone = {
     connectOscillator: (oscillator) => {
       oscillator.connect(gainNode);
     },
     nw: function () {
-      this.connectOscillator(nw)
+      this.connectOscillator(nw);
     },
     ne: function () {
-      this.connectOscillator(ne)
+      this.connectOscillator(ne);
     },
     sw: function () {
-      this.connectOscillator(sw)
+      this.connectOscillator(sw);
     },
     se: function () {
-      this.connectOscillator(se)
+      this.connectOscillator(se);
     }
   };
 
-  // ******* Methods for stopping each audio tone *******
-
+  /**
+   * Methods for stopping each oscillator tone
+   */
   let stopTone = {
     stop: (oscillator) => {
-      currGain = 0
-      oscillator.disconnect(gainNode)
+      currGain = 0;
+      oscillator.disconnect(gainNode);
     },
     nw: function () {
-      this.stop(nw)
+      this.stop(nw);
     },
     ne: function () {
-      this.stop(ne)
+      this.stop(ne);
     },
     sw: function () {
-      this.stop(sw)
+      this.stop(sw);
     },
     se: function () {
-      this.stop(se)
+      this.stop(se);
     }
   };
 
   return {
-  	nw: nw,
-  	ne: ne,
-  	sw: sw,
-  	se: se,
-  	startTone: startTone,
-  	stopTone: stopTone
-  }
-
+    nw: nw,
+    ne: ne,
+    sw: sw,
+    se: se,
+    startTone: startTone,
+    stopTone: stopTone
+  };
 })();
 
 const simon = (function () {
-  
-  // ******* Game State *******
-
+  /**
+   * Game State
+   */
   const colors = {
     nw: '#080',
     ne: '#F00',
@@ -107,24 +111,24 @@ const simon = (function () {
   let strictMode = false;
   let playerSecondChance = false;
 
-  function getRandomInt(min, max) {
+  function getRandomInt (min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
   const simonLogic = {
 
-		updateDisplay: function (value) {
-			$('#display')
+    updateDisplay: function (value) {
+      $('#display')
         .empty()
         .append(value);
-      },
+    },
 
     /**
      * Runs recursivly until each index in computerSteps is played through
      */
     playComputerSteps: function (index) {
       if (!computerSteps[index]) {
-        simonLogic.playerTurn();
+        this.playerTurn();
         return;
       }
 
@@ -133,7 +137,6 @@ const simon = (function () {
       const activeColor = colorsActive[computerSteps[index]];
       const defaultColor = colors[computerSteps[index]];
 
-
       const displayTurns = () => {
         return new Promise((resolve, reject) => {
           setTimeout(() => {
@@ -141,12 +144,12 @@ const simon = (function () {
             audio.startTone[currentStep]();
             resolve();
           }, 500);
-        })
-      }
+        });
+      };
 
       displayTurns()
       .then(() => {
-        return new Promise(function(resolve, reject) {
+        return new Promise((resolve, reject) => {
           setTimeout(() => {
             $(stepId).css('background', defaultColor);
             audio.stopTone[currentStep]();
@@ -155,14 +158,13 @@ const simon = (function () {
         });
       })
       .then(() => {
-          this.playComputerSteps(index + 1);
+        this.playComputerSteps(index + 1);
       });
     },
 
     computerTurn: function () {
-
       if (computerSteps.length === 20) {
-        return simonLogic.gameWin();
+        return this.gameWin();
       }
 
       if (!playerSecondChance) {
@@ -171,47 +173,45 @@ const simon = (function () {
         computerSteps.push(availableSteps[randomButtonIndex]);
       }
 
-      simonLogic.updateDisplay(computerSteps.length);
-    
+      this.updateDisplay(computerSteps.length);
+
       this.playComputerSteps(0);
     },
 
     playerTurn: function () {
-
      // make simon-buttons clickable
-     $('.simon-buttons')
-     	.addClass('clickable')
-     	.mousedown(function(evt) {
-     		$('#' + evt.target.id).css('background', colorsActive[evt.target.id]);
-        console.log('PLAYER TONE STARTING', evt)
-     		audio.startTone[evt.target.id]();
-     	})
-     	.mouseup(function(evt) {
-     		$('#' + evt.target.id).css('background', colors[evt.target.id]);
-        console.log('PLAYER TONE ENDING')
-     		audio.stopTone[evt.target.id]();
-     	})
-     	.on('click', function(evt) {
-      	evt.preventDefault();
-      	evt.stopPropagation();
-      	var currentPlayerStepIndex;
+      $('.simon-buttons')
+      .addClass('clickable')
+      .mousedown((evt) => {
+        $('#' + evt.target.id).css('background', colorsActive[evt.target.id]);
+        console.log('PLAYER TONE STARTING', evt);
+        audio.startTone[evt.target.id]();
+      })
+      .mouseup((evt) => {
+        $('#' + evt.target.id).css('background', colors[evt.target.id]);
+        console.log('PLAYER TONE ENDING');
+        audio.stopTone[evt.target.id]();
+      })
+      .on('click', (evt) => {
+        evt.preventDefault();
+        evt.stopPropagation();
+        var currentPlayerStepIndex;
 
-      	playerSteps.push(evt.target.id);
-      	currentPlayerStepIndex = playerSteps.length - 1;
+        playerSteps.push(evt.target.id);
+        currentPlayerStepIndex = playerSteps.length - 1;
 
-      	if (computerSteps[currentPlayerStepIndex] === playerSteps[currentPlayerStepIndex]) {
-
-      		if (computerSteps.length === playerSteps.length) {
-      			$('.simon-buttons')
-      				.off()
-      				.removeClass('clickable');
-      			playerSteps = [];
+        if (computerSteps[currentPlayerStepIndex] === playerSteps[currentPlayerStepIndex]) {
+          if (computerSteps.length === playerSteps.length) {
+            $('.simon-buttons')
+              .off()
+              .removeClass('clickable');
+            playerSteps = [];
 
             if (playerSecondChance) {
-              simonLogic.clearAnotherChance();
+              this.clearAnotherChance();
             }
 
-            return simonLogic.computerTurn();
+            return this.computerTurn();
           }
 
           $('.simon-buttons')
@@ -219,7 +219,7 @@ const simon = (function () {
               .removeClass('clickable');
 
           return simonLogic.playerTurn();
-        } 
+        }
         // if strictMode is false, player gets another chance
         if (!strictMode) {
           simonLogic.anotherChance();
@@ -229,7 +229,7 @@ const simon = (function () {
           playerSteps = [];
           return simonLogic.computerTurn();
         } else {
-        // else, strictMode is true, which means game over    
+        // else, strictMode is true, which means game over
           simonLogic.gameLose();
           $('.simon-buttons')
             .off()
@@ -239,23 +239,22 @@ const simon = (function () {
       });
     },
 
-
-    clearMoves: function() {
+    clearMoves: function () {
       computerSteps = [];
       playerSteps = [];
       playerSecondChance = false;
     },
 
-    resetGame: function() {
-      simonLogic.clearMoves();
-      simonLogic.updateDisplay('--');
+    resetGame: function () {
+      this.clearMoves();
+      this.updateDisplay('--');
       $('.simon-buttons')
         .off()
         .removeClass('clickable');
       $('#reset-button').off('click');
     },
 
-    anotherChance: function() {
+    anotherChance: function () {
       $('.game-status')
         .empty()
         .append('<h2>WRONG! TRY AGAIN</h2>');
@@ -263,76 +262,74 @@ const simon = (function () {
       playerSecondChance = true;
     },
 
-    clearAnotherChance: function() {
-      simonLogic.clearGameStatus();
+    clearAnotherChance: function () {
+      this.clearGameStatus();
       playerSecondChance = false;
     },
 
-    gameWin: function() {
+    gameWin: function () {
       $('.game-status').append('<h2>YOU WIN!</h2>');
-      return setTimeout(function() {
-        simonLogic.resetGame();
-        simonLogic.clearGameStatus();
-        simonLogic.computerTurn();
+      return setTimeout(() => {
+        this.resetGame();
+        this.clearGameStatus();
+        this.computerTurn();
       }, 1000);
     },
 
-    gameLose: function() {
+    gameLose: function () {
       $('.game-status').empty().append('<h2>YOU LOSE!</h2>');
       return buttonLogic.start();
     },
 
-    clearGameStatus: function() {
+    clearGameStatus: function () {
       $('.game-status').empty();
     }
   };
-  
+
   const buttonLogic = {
     start: function () {
-      $('#start-button').click( () => {   
+      $('#start-button').click(() => {
         simonLogic.clearMoves();
         simonLogic.clearGameStatus();
         simonLogic.computerTurn();
         $('#start-button').off('click');
-        return buttonLogic.reset();
+        return this.reset();
       });
     },
-    
-    reset: function() {
-      $('#reset-button').click( () => {
+
+    reset: function () {
+      $('#reset-button').click(() => {
         simonLogic.resetGame();
         simonLogic.clearGameStatus();
         $('#reset-button').off('click');
-        return buttonLogic.start();
+        return this.start();
       });
     },
 
-    strict: function() {
-      $('#strict-mode-button').click( () => {
+    strict: function () {
+      $('#strict-mode-button').click(() => {
         strictMode = !strictMode;
-        buttonLogic.strictLightToggle();
+        this.strictLightToggle();
       });
     },
 
-    strictLightToggle: function() {
+    strictLightToggle: function () {
       $('#strict-mode-light').toggleClass('on');
     }
 
   };
-  
+
   function init () {
     buttonLogic.start();
-    //buttonLogic.reset();
     buttonLogic.strict();
   }
-  
+
   return {
     init: init,
     getRandomInt: getRandomInt
   };
-  
 })();
 
-$(document).ready(function() {
+$(document).ready(function () {
   simon.init();
 });
